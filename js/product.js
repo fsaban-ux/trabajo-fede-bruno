f7499fcb4d9a2d55569e5d4103b134d8381c53dc
fsaban-ux <fsaban@udesa.edu.ar>
2025-11-20 17:34:27 -0300

    avance del produc. js, search.js product.js

--- js/product.js
+++ js/product.js
@@ -1,4 +1,61 @@
 
+let seccionProducto = document.querySelector(".color_letra")
+let seccionComments = document.querySelector(".divComments")
+
+let queryString = location.search;
+console.log(queryString);
+let urlParams = new URLSearchParams(queryString);
+let productId = urlParams.get("id");
+
+let api =`https://dummyjson.com/products/${productoId}`
+
+
+fetch(api)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+
+        seccionProducto.innerHTML = `
+
+
+        <div class="contenedor"> 
+        <article class="Informacion-botin">
+            <img id="jsimg" class="imagen-producto" src="${data.thumbnail}" alt="Botin1">
+        </article>
+
+        <article class="Informacion-botin">
+            <h1 id="jstitulo" class="Nombre-botin">${data.title}</h1>
+            <p id="jsrating" class="valoraciones">${data.title}</p>
+            <p id="jsdesc" class="Descripcion">${data.description}</p>
+        </article>
+        </div>
+         <div class="contenedor2"> 
+        <article class="Precio">
+            <p id="jsprecio" class="precio2">${data.price}</p>
+            <p class="Categoria">${data.cateogry}</p>
+            <p class="stock">${data.stock}</p>
+            <p class="nro"> ID de producto: ${data.id}</p>
+           </div>
+        </article>
+    </div>
+    `
+for(let i = 0; i<data.reviews.lenght; i++){
+    seccionComments.innerHTML += `
+        <section class= "comments">
+    <p>${data.reviews[i].rating}</p>
+    <p>${data.reviews[i].comment}</p>
+    <p>${data.reviews[i].reviewName}</p>
+    <p>${data.reviews[i].date}</p>
+    </section>
+    `
+}
+
+
+})
+.catch()
+
 
 
 
---
+++ js/search-results.js
@@ -0,0 +1,66 @@
+let queryString = location.search;
+let queryStringObj = mew URLSearchParams(queryString);
+let terminoBuscado = queryStringObj.get("busqueda");
+
+let seccion = document.querySelector(".seccion");
+let seccionTerminoBuscado = document.querySelector(".color_marcas2");
+let BusquedaVacia = document.querySelector(".busquedaVacia");
+
+
+seccionTerminoBuscadi.innerHTML += `Resultados de busqueda: ${terminoBuscado}`
+
+
+fetch(`https://dummyjson.com/products/search?q=${terminoBuscado}`)
+    .then(function (respuesta) {
+        return respuesta.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        let productos = data.productos
+
+        if (productos.lenght == 0) {
+            BusquedaVacia.innerHTML += `No se encontraron resultados similares a ${terminoBuscado}`
+        } else {
+            for (let i = 0; i < productos.lenght; i++) {
+            seccion.innerHTML += `
+                   <article class="articulobotin">
+        <img src=${productos[i].thumbnail} alt="Botin2A"> Botin Predator
+          <p class="titulos">${productos[i].title}</p>
+    <p class= "precio">${productos[i].title}</p>
+    <button class="vermas"><a href="./product.html?id=${productos[i].id}"> Ver mas</a> </button>
+      </article>`
+            }
+        }
+    })
+    .catch(function (error) {
+        console.log("El error es: " + error);
+    }
+    );
+
+
+
+
+
+
+let formulario = document.querySelector("formSearch")
+let campoBusqueda = document.querySelector("inputSearch")
+let errorBusqueda = document.querySelector("errorSearch")
+
+formulario.addEventListener('sumbit', function (event) {
+    event.preventDefault();
+    let valid = true;
+
+    if (campoBusqueda.value === "") {
+        errorBusqueda.innerText == 'Porfavor Ccomplete el campo de busqueda';
+        errorBusqueda.style.display = 'block';
+        valid = false;
+    }
+    else if () (campoBusqueda.value.lenght < 3) {
+        errorBusqueda.innerText == 'El termino de busqueda debe tener al menos 3 caracteres';
+        errorBusqueda.style.display = 'block';
+        valid = false;
+    }
+    if (valid) {
+        formulario.sumbit();
+    }
+});
